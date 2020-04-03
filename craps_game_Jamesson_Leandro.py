5b40a95e93eaf64a8172b38aff04825fc7eb5134
jamessonlps <jamessonlps@al.insper.edu.br>
2020-04-02 22:24:30 -0300

    craps_game_Jamesson_Leandro.py

--- craps_game_Jamesson_Leandro.py
+++ craps_game_Jamesson_Leandro.py
@@ -149,13 +149,65 @@ while jogo_valendo:
                             print('Além disso, você só sairá dessa fase tirando o Point ou o 7.')
                             print('')
                             print('Agora, suas opções de aposta são as mesmas, exceto a Pass Line Bet, lembrando da regra acima do Points.')
-                            print('Você deve escolher o tipo de aposta que irá começar nessa fase.')
                             print('')
-                            print('1. Field - para escolhê-la, digite "field"')
-                            print('2. Any Craps - para escolhê-la, digite "craps"')
-                            print('3. Twelve - para escolhê-la, digite "twelve"')
-                            resposta3 = input('Sua escolha: ')
                             points_valendo = True
                             Point = soma_dados
 
-                            while points_valendo:
+                            while points_valendo:            # Aqui inicia o ciclo de Points até que o jogador ganhe ou perca no Points ou fique sem fichas nas outras apostas
+
+                                if fichas_em_maos == 0:
+                                    print('Suas fichas acabaram!')
+                                    print('Sentimos muito, mas você perdeu tudo. Agradecemos a sua participação!')
+                                    points_valendo = False
+                                    jogo_valendo = False
+                                else:                                    
+                                    print('Você deve escolher alguma das três apostas ou ficar jogando "points" até que ganhe sua aposta ou perca tudo.')
+                                    print('1. Field - para escolhê-la, digite "field"')
+                                    print('2. Any Craps - para escolhê-la, digite "craps"')
+                                    print('3. Twelve - para escolhê-la, digite "twelve"')
+                                    print('4. Digite "points" para tentar marcar o Point')
+                                    resposta3 = input('Sua escolha: ')
+                                    print('')
+                                    dado_1 = random.randint(1, 6)
+                                    dado_2 = random.randint(1, 6)
+                                    nova_soma_dados = dado_1 + dado_2
+
+                                    if resposta3 == 'field':
+                                        print('Você escolheu Field. Vejamos a sua sorte...')
+                                        print('')
+                                        
+                                        if nova_soma_dados == 5 or nova_soma_dados == 6 or nova_soma_dados == 7 or nova_soma_dados == 8:
+                                            print('É o fim da linha para você, lamentamos. Obrigado por jogar conosco')
+                                            points_valendo = False
+                                            jogo_valendo = False
+                                        elif nova_soma_dados == 3 or soma_dados == 4 or soma_dados == 9 or soma_dados == 10 or soma_dados == 11:
+                                            print('Parabéns, você ganhou o valor da sua aposta')
+                                            fichas_em_maos = fichas_em_maos + aposta_jogador
+                                            print('Agora você tem {0} fichas'.format(fichas_em_maos))
+                                            print('')
+                                        elif nova_soma_dados == 2:
+                                            print('Parabéns, você ganhou o dobro da sua aposta')
+                                            fichas_em_maos = fichas_em_maos + 2*aposta_jogador
+                                            print('Agora você tem {0} fichas'.format(fichas_em_maos))
+                                            print('')
+                                        elif nova_soma_dados == 12:
+                                            print('Bingo! Os dados somaram 12 e você leva o triplo da aposta')
+                                            fichas_em_maos = fichas_em_maos + 3*aposta_jogador
+                                            print('Agora você tem {0} fichas'.format(fichas_em_maos))
+                                            print('')
+
+                                    elif resposta3 == 'craps':
+                                        print('Você escolheu Any Craps. Se seus dados somarem 2, 3 ou 12, parabéns. Se não, você perde.')
+                                        print('')
+                                        
+                                        if soma_dados == 2 or soma_dados == 3 or soma_dados == 12:
+                                            print('Woooow, você ganhou!!!)
+                                            fichas_em_maos = fichas_em_maos + 7*aposta_jogador
+                                            valor_ganho = 7*aposta_jogador
+                                            print('Você ganhou {0} fichas. Agora, você tem {1} fichas'.format(valor_ganho, fichas_em_maos))
+                                            print('')
+                                        else:
+                                            print('Uma pena, você perdeu sua aposta.')
+                                            fichas_em_maos = fichas_em_maos - aposta_jogador
+                                            print('Agora, você tem {0} fichas'.format(fichas_em_maos))
+                                            print('')
