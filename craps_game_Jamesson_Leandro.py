89567488003107819c3efe97485f984bec1f1bc9
jamessonlps <jamessonlps@al.insper.edu.br>
2020-04-03 09:49:02 -0300

    craps_game_Jamesson_Leandro.py

--- craps_game_Jamesson_Leandro.py
+++ craps_game_Jamesson_Leandro.py
@@ -14,8 +14,9 @@ fichas_em_maos = 30  # O jogador começa com 30 fichas
 # Importando biblioteca random
 import random
 
+print('--------------------------------------------------------------------------------------------------------------------------------------')
 print('Bem vindo(a) ao grande Cassino James. Se prepare para uma jornada no nosso Craps game e esteja dispoto(a) a apostar suas fichas, hein?')
-print('')
+print('--------------------------------------------------------------------------------------------------------------------------------------')
 
 print('Você começará esse jogo com 30 fichas, mas tente sair daqui com pelo menos 200 delas, ou vai sair sem nada...')
 print('')
@@ -39,11 +40,11 @@ while jogo_valendo:
             jogo_valendo = False
         elif resposta1 == 'sim':
             print('Boa decisão. Começaremos com a 1° fase chamada "Come Out". Você tem quatro escolhas de apostas:')
+            print('')
             print('1. Pass Line Bet - para escolhê-la, digite "line bet"')
             print('2. Field - para escolhê-la, digite "field"')
             print('3. Any Craps - para escolhê-la, digite "craps"')
             print('4. Twelve - para escolhê-la, digite "twelve"')
-            print('')
             resposta2 = input('Sua escolha: ')
             print('')
             print('Agora, precisamos que você digite o número de fichas que apostará.')
@@ -62,13 +63,14 @@ while jogo_valendo:
                     print('')
 
                     if soma_dados == 12:
-                        print('Woooow, você ganhou!!!')
+                        print('Woooow, você ganhou!!! A soma dos dados deu {0}!'.format(soma_dados))
+                        print('')
                         fichas_em_maos = fichas_em_maos + 30*aposta_jogador
                         valor_ganho = 30*aposta_jogador
                         print('Você ganhou {0} fichas. Agora, você tem {1} fichas'.format(valor_ganho, fichas_em_maos))
                         print('')
                     else:
-                        print('Uma pena, você perdeu sua aposta.')
+                        print('Uma pena, você perdeu sua aposta. A soma dos dados deu {0}!'.format(soma_dados))
                         print('')
                         fichas_em_maos = fichas_em_maos - aposta_jogador
                         print('Agora, você tem {0} fichas'.format(fichas_em_maos))
@@ -82,13 +84,14 @@ while jogo_valendo:
                     print('')
 
                     if soma_dados == 2 or soma_dados == 3 or soma_dados == 12:
-                        print('Woooow, você ganhou!!!')
+                        print('Woooow, você ganhou!!! A soma dos dados deu {0}!'.format(soma_dados))
+                        print('')
                         fichas_em_maos = fichas_em_maos + 7*aposta_jogador
                         valor_ganho = 7*aposta_jogador
                         print('Você ganhou {0} fichas. Agora, você tem {1} fichas'.format(valor_ganho, fichas_em_maos))
                         print('')
                     else:
-                        print('Uma pena, você perdeu sua aposta.')
+                        print('Uma pena, você perdeu sua aposta. A soma dos dados deu {0}!'.format(soma_dados))
                         print('')
                         fichas_em_maos = fichas_em_maos - aposta_jogador
                         print('Agora, você tem {0} fichas'.format(fichas_em_maos))
@@ -103,21 +106,24 @@ while jogo_valendo:
                     print('')
                     
                     if soma_dados == 5 or soma_dados == 6 or soma_dados == 7 or soma_dados == 8:
-                        print('Não foi dessa vez, você perdeu absolutamente tudo!')
+                        print('Não foi dessa vez, você perdeu absolutamente tudo! A soma dos dados deu {0}!'.format(soma_dados))
+                        print('')
                         fichas_em_maos = 0
-                        jogo_valendo = False
                     elif soma_dados == 3 or soma_dados == 4 or soma_dados == 9 or soma_dados == 10 or soma_dados == 11:
-                        print('Você ganhou o equivalente à sua aposta!')
+                        print('Você ganhou o equivalente à sua aposta! A soma dos dados deu {0}'.format(soma_dados))
+                        print('')
                         fichas_em_maos = fichas_em_maos + aposta_jogador
                         print('Você agora tem {0} fichas'.format(fichas_em_maos))
                         print('')
                     elif soma_dados == 2:
-                        print('Você ganhou o dobro da sua aposta!')
+                        print('Você ganhou o dobro da sua aposta! A soma dos dados deu 2!')
+                        print('')
                         fichas_em_maos = fichas_em_maos + 2*aposta_jogador
                         print('Você agora tem {0} fichas'.format(fichas_em_maos))
                         print('')
                     else:   # Isto é, tira 12
-                        print('Você ganhou o triplo da sua aposta!')
+                        print('Você ganhou o triplo da sua aposta! A soma dos dados deu 12!')
+                        print('')
                         fichas_em_maos = fichas_em_maos + 3*aposta_jogador
                         print('Você agora tem {0} fichas'.format(fichas_em_maos))
                         print('')
@@ -132,12 +138,14 @@ while jogo_valendo:
                     print('')
 
                     if soma_dados == 7 or soma_dados == 11:
-                        print('Muito bem, você ganhou essa!')
+                        print('Muito bem, você ganhou essa! A soma dos dados deu {0}!'.format(soma_dados))
+                        print('')
                         fichas_em_maos = fichas_em_maos + aposta_jogador
                         print('Agora você tem {0} fichas'.format(fichas_em_maos))
                         print('')
                     elif soma_dados == 2 or soma_dados == 3 or soma_dados == 12:
-                        print('CRAPS! Você perdeu sua aposta :(')
+                        print('CRAPS! Você perdeu sua aposta. A soma dos dados deu {0} :('.format(soma_dados))
+                        print('')
                         fichas_em_maos = fichas_em_maos - aposta_jogador
                         print('Agora você tem {0} fichas'.format(fichas_em_maos))
                         print('')
@@ -151,7 +159,7 @@ while jogo_valendo:
                         points_valendo = True
                         Point = soma_dados
 
-                        while points_valendo:            # Aqui inicia o ciclo de Points até que o jogador ganhe ou perca no Points ou fique sem fichas nas outras apostas
+                        while points_valendo:            # Aqui inicia o ciclo da fase Points até que o jogador ganhe ou perca no Points ou fique sem fichas nas outras apostas
 
                             if fichas_em_maos == 0:
                                 print('Suas fichas acabaram!')
@@ -175,21 +183,24 @@ while jogo_valendo:
                                     print('')
                                     
                                     if nova_soma_dados == 5 or nova_soma_dados == 6 or nova_soma_dados == 7 or nova_soma_dados == 8:
-                                        print('É o fim da linha para você, lamentamos. Obrigado por jogar conosco')
+                                        print('É o fim da linha para você, lamentamos. A soma dos dados deu {0} :('.format(nova_soma_dados))
                                         points_valendo = False
-                                        jogo_valendo = False
-                                    elif nova_soma_dados == 3 or soma_dados == 4 or soma_dados == 9 or soma_dados == 10 or soma_dados == 11:
-                                        print('Parabéns, você ganhou o valor da sua aposta')
+                                        print('')
+                                    elif nova_soma_dados == 3 or nova_soma_dados == 4 or nova_soma_dados == 9 or nova_soma_dados == 10 or nova_soma_dados == 11:
+                                        print('Parabéns, você ganhou o valor da sua aposta! A soma dos dados deu {0}!'.format(nova_soma_dados))
+                                        print('')
                                         fichas_em_maos = fichas_em_maos + aposta_jogador
                                         print('Agora você tem {0} fichas'.format(fichas_em_maos))
                                         print('')
                                     elif nova_soma_dados == 2:
-                                        print('Parabéns, você ganhou o dobro da sua aposta')
+                                        print('Parabéns, você ganhou o dobro da sua aposta! A soma dos dados deu {0}!'.format(nova_soma_dados))
+                                        print('')
                                         fichas_em_maos = fichas_em_maos + 2*aposta_jogador
                                         print('Agora você tem {0} fichas'.format(fichas_em_maos))
                                         print('')
                                     elif nova_soma_dados == 12:
-                                        print('Bingo! Os dados somaram 12 e você leva o triplo da aposta')
+                                        print('Bingo! Os dados somaram 12 e você leva o triplo da aposta! A soma dos dados deu {0}!'.format(nova_soma_dados))
+                                        print('')
                                         fichas_em_maos = fichas_em_maos + 3*aposta_jogador
                                         print('Agora você tem {0} fichas'.format(fichas_em_maos))
                                         print('')
@@ -199,13 +210,15 @@ while jogo_valendo:
                                     print('')
                                     
                                     if nova_soma_dados == 2 or nova_soma_dados == 3 or nova_soma_dados == 12:
-                                        print('Woooow, você ganhou!!!')
+                                        print('Woooow, você ganhou!!! A soma dos dados deu {0}!'.format(nova_soma_dados))
+                                        print('')
                                         fichas_em_maos = fichas_em_maos + 7*aposta_jogador
                                         valor_ganho = 7*aposta_jogador
                                         print('Você ganhou {0} fichas. Agora, você tem {1} fichas'.format(valor_ganho, fichas_em_maos))
                                         print('')
                                     else:
-                                        print('Uma pena, você perdeu sua aposta.')
+                                        print('Uma pena, você perdeu sua aposta. A soma dos dados deu {0}!'.format(nova_soma_dados))
+                                        print('')
                                         fichas_em_maos = fichas_em_maos - aposta_jogador
                                         print('Agora, você tem {0} fichas'.format(fichas_em_maos))
                                         print('')
@@ -215,13 +228,14 @@ while jogo_valendo:
                                     print('')
 
                                     if nova_soma_dados == 12:
-                                        print('Woooow, você ganhou!!!')
+                                        print('Woooow, você ganhou!!! A soma dos dados deu 12!')
+                                        print('')
                                         fichas_em_maos = fichas_em_maos + 30*aposta_jogador
                                         valor_ganho = 30*aposta_jogador
                                         print('Você ganhou {0} fichas. Agora, você tem {1} fichas'.format(valor_ganho, fichas_em_maos))
                                         print('')
                                     else:
-                                        print('Uma pena, você perdeu sua aposta.')
+                                        print('Uma pena, você perdeu sua aposta. A soma dos dados deu {0}!'.format(nova_soma_dados))
                                         fichas_em_maos = fichas_em_maos - aposta_jogador
                                         print('Agora, você tem {0} fichas'.format(fichas_em_maos))
                                         print('')
@@ -231,16 +245,18 @@ while jogo_valendo:
                                     print('')
 
                                     if nova_soma_dados == Point:
-                                        print('Na mosca! Você ganhou a sua aposta e começará uma nova rodada na fase "Come Out".')
+                                        print('Na mosca, o Point era {0}! Você ganhou a sua aposta e começará uma nova rodada na fase "Come Out".'.format(nova_soma_dados))
+                                        print('')
                                         fichas_em_maos = fichas_em_maos + aposta_jogador
                                         print('Agora você tem {0} fichas.'.format(fichas_em_maos))
                                         points_valendo = False
                                         print('')
                                     elif nova_soma_dados == 7:
-                                        print('Esse foi o maior azar da sua noite! Você perdeu tudo!! Por hoje é isso, até a próxima!')
+                                        print('Esse foi o maior azar da sua noite, seus dados somaram 7!')
+                                        print('')
                                         fichas_em_maos = 0
                                         points_valendo = False
-                                        jogo_valendo = False
                                     else:
                                         print('Parece que não deu em nada, você não ganhou nem perdeu... Por enquanto.')
+                                        print('')
                                             
